571a5f4b540e77b34f1ba831591bc8d56ea60bc3
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-11 18:11:40 +0100

    Added the Solarized colour theme to siancu-pack.

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; Init file for my personal emacs-live pack
 
+;; Load the solarized color theme
+(live-load-config-file "solarized-conf.el")
+
 ;; Load the org config
 (live-load-config-file "org-mode-conf.el")
 
