d93f45a61d87506b733f0a13bc07d95d0e6c3596
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-11 16:20:48 +0100

    Added the contrib dir to load-path.

--- config/org-mode-conf.el
+++ config/org-mode-conf.el
@@ -1,6 +1,11 @@
 ;; Generic org-mode configuration
 
+;; Add org-mode itself to load-path
 (live-add-pack-lib "org-mode/lisp/")
+
+;; Add the contrib dir to load-path
+(live-add-pack-lig "org-mode/contrib/lisp/")
+
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
