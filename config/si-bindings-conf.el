5b34ccd4ce90b900c4350926b33f7cb765da5c2f
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-12 23:50:11 +0100

    Added more key bindings.

--- config/si-bindings-conf.el
+++ config/si-bindings-conf.el
@@ -8,3 +8,47 @@
 
 ;; Ace-jump mode
 (global-set-key (kbd "C-o") 'ace-jump-mode)
+
+;;browse kill ring (visual paste)
+(global-set-key (kbd "M-y") 'browse-kill-ring)
+
+;; undo
+(global-set-key (kbd "C-M-/") 'undo-tree-undo)
+
+;;scroll other window
+(global-set-key (kbd "C-M-]") 'scroll-other-window)
+(global-set-key (kbd "C-M-[") 'scroll-other-window-down)
+
+;;fast vertical naviation
+(global-set-key  (kbd "M-U") (lambda () (interactive) (forward-line -10)))
+(global-set-key  (kbd "M-D") (lambda () (interactive) (forward-line 10)))
+(global-set-key  (kbd "M-p") 'outline-previous-visible-heading)
+(global-set-key  (kbd "M-n") 'outline-next-visible-heading)
+
+;; Align your code in a pretty way.
+(global-set-key (kbd "C-x \\") 'align-regexp)
+
+;; comment region
+(global-set-key (kbd "M-/") 'comment-or-uncomment-region)
+
+(global-set-key (kbd "C-s") 'isearch-forward)
+(global-set-key (kbd "C-r") 'isearch-backward)
+(global-set-key (kbd "C-M-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-r") 'isearch-backward-regexp)
+
+;; Jump to a definition in the current file.
+(global-set-key (kbd "C-x C-i") 'idomenu)
+
+;; File
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Activate occur easily inside isearch
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
