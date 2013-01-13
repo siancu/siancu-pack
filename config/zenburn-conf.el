ba8fb77cebdc6b1e85767b0e09d3bb41f6646642
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-13 22:04:17 +0100

    Updated siancu-pack.

--- config/zenburn-conf.el
+++
@@ -1,5 +0,0 @@
-;; Solarized theme configuration file
-
-(live-add-pack-lib "zenburn")
-(require 'zenburn-theme)
-(load-theme 'zenburn t)
--- init.el
+++ init.el
@@ -1,13 +1,4 @@
 ;; Init file for my personal emacs-live pack
 
-;; Load the solarized color theme
-;;(live-load-config-file "solarized-conf.el")
-
-;; Load the zenburn color theme
-(live-load-config-file "zenburn-conf.el")
-
-;; Load the org config
-(live-load-config-file "org-mode-conf.el")
-
 ;; Load the bindings config
 (live-load-config-file "si-bindings-conf.el")
